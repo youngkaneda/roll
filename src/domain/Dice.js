a571b169eb62f9f224cd8c6fc0733a15f70de62a
shotaro kaneda <juanpablolvl99@gmail.com>
2020-05-21 10:55:07 -0300

    Adding a log of results.

--- src/domain/Dice.js
+++ src/domain/Dice.js
@@ -6,23 +6,13 @@ function Dice(sides) {
         throw new Error('Invalid value for dice sides.');
     }
     this.sides = parseInt(sides);
-    /* functions */
-    let range = (limit) => {
-        return [...Array(limit).keys()];
-    }
+
     let random = (limit) => {
         return 1 + Math.floor(Math.random() * limit);
     }
-    /* */
+
     this.roll = function() {
-        if (arguments.length === 0) {
-            return random(this.sides);
-        }
-        const times = parseInt(arguments[0]);
-        if (times < 1) {
-            throw new Error('Invalid parameter value. The dice can roll one or more times.');
-        }
-        return range(times).map(t => this.roll()).reduce((p, c) => p + c);
+        return random(this.sides);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@
 
 const Dice = require(__dirname + '/domain/Dice');
 const args = process.argv.slice(2);
+const range = (limit) => {
+    return [...Array(limit).keys()];
+}
 
-const dices = args[0].split(',');
+Array.prototype.flatMap = function(lambda) {
+    return Array.prototype.concat.apply([], this.map(lambda));
+}
+
+let dices = args[0].split(',');
 const modifier = args[1] ? parseInt(args[1]) : 0;
 
 const result = dices.map(dice => dice.split('d'))
     .filter(arr => arr.length === 2)
-    .map(arr => new Dice(arr[1]).roll(arr[0]))
-    .reduce((p, c) => p + c, 0);
+    .flatMap(arr => range(parseInt(arr[0])).map(v => new Dice(arr[1])))
+    .map(dice => { return { type: `d${dice.sides}`, value: dice.roll() } });
 
-console.log(result + modifier);
+if (result.length > 0) {
+    console.log(result.map((obj) => `${obj.type} => ${obj.value}`).join(', '));
+}
+console.log('total =>', result.map(obj => obj.value).reduce((p, c) => p + c, 0) + modifier);
+console.log('applied modifier =>', modifier);
